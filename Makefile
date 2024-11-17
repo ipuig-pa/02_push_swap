5681d8a6ea01c1eb1b597a5c86ece0a8bbbda8ac
Irene Puig Parnau <ipuig-pa@3-F-11.42heilbronn.de>
2024-11-17 12:41:27 +0100

    try to add bonus

--- Makefile
+++ Makefile
@@ -11,19 +11,31 @@ SOURCES = 	push_swap.c \
 			push_swap_helper.c \
 			push_swap_stack_tools.c
 
-BONUS_SOURCES = checker_bonus.c
+BONUS_SOURCES = push_swap_checker_bonus.c \
+				push_swap_stack_creation.c \
+				push_swap_sort.c \
+				push_swap_exec_and_print.c \
+				push_swap_commands.c \
+				push_swap_helper.c \
+				push_swap_stack_tools.c
 
 OBJECTS = $(SOURCES:.c=.o)
 HEADER = push_swap.h
 
+BONUS_OBJECTS = $(BONUS_SOURCES:.c=.o)
+BONUS_HEADER = push_swap_bonus.h
+
 LIBFT_DIR = libft
 LIBFT = $(LIBFT_DIR)/libft.a
 
-#get next line, ft printf, bonus, etc
-
 all : $(NAME)
 
-$(NAME) : $(OBJECTS) $(LIBFT)
+bonus : $(BONUS_NAME)
+
+$(BONUS_NAME) : $(BONUS_OBJECTS) $(LIBFT) $(BONUS_HEADER)
+	cc $(CFLAGS) -o $(BONUS_NAME) $(BONUS_OBJECTS) $(LIBFT)
+
+$(NAME) : $(OBJECTS) $(LIBFT) $(HEADER)
 	cc $(CFLAGS) -o $(NAME) $(OBJECTS) $(LIBFT)
 
 $(LIBFT) : 
@@ -32,17 +44,12 @@ $(LIBFT) :
 %o: %.c $(HEADER)
 	cc $(CFLAGS) -c $< -o $@
 
-bonus : $(BONUS_NAME)
-
-$(BONUS_NAME) : $(BONUS) $(NAME)
-
-
 clean:
-	rm -f $(OBJECTS)
+	rm -f $(OBJECTS) $(BONUS_OBJECTS)
 	make -C $(LIBFT_DIR) clean
 
 fclean: clean
-	rm -f $(NAME)
+	rm -f $(NAME) $(BONUS_NAME)
 	make -C $(LIBFT_DIR) fclean
 
 re: fclean all
---
+++ push_swap_bonus.h
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 10:30:02 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2024/11/17 12:39:04 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_BONUS_H
+# define PUSH_SWAP_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int				content;
+	int				index;
+	struct s_stack	*next;
+}	t_stack;
+
+int		main(int argc, char **argv);
+
+int		valid_input_to_arr(int count, char **argv, int *arr, int flag);
+t_stack	*parse_in_stack(int argc, int *arr);
+t_stack	*assign_index(t_stack *stack);
+
+void	sort_stack_a(t_stack **st_a);
+void	small_size_sort(int size, t_stack **st_a);
+void	middle_size_sort(int size, t_stack **st_a);
+void	big_size_sort(int size, t_stack **st_a);
+t_stack	**move_to_b(int size, t_stack **st_a, t_stack **st_b);
+void	back_to_a(int size, t_stack **st_a, t_stack **st_b);
+void	prepare_push(t_stack **st_a, t_stack **st_b, int ind, char id);
+
+void	exec_and_print(char *comm, char st_id, t_stack **st_a, t_stack **st_b);
+char	*print(char *prev_comm, char *comm, char prev_id, char st_id);
+
+void	swap(t_stack **stack);
+void	push(t_stack **stack_destination, t_stack **stack_source);
+void	rotate(t_stack **stack);
+void	reverse_rotate(t_stack **stack);
+
+int		handle_error(int flag, char **argv, int *arr, int argc);
+int		arrlen(char **arr);
+int		is_int(char *str);
+int		is_arr_order(int *arr, int size);
+int		ft_sqrt(int num);
+t_stack	*ft_lstlast(t_stack *stack);
+int		ft_lstsize(t_stack *stack);
+int		ft_lstpos(t_stack *current, t_stack *interrogated);
+int		is_order(t_stack *stack);
+t_stack	*clear_stack(t_stack *stack);
+
+int		push_swap(int argc, char **argv);
+int		read_and_exec(t_stack **st_a, t_stack **st_b);
+int		exec(char *comm, char st_id, t_stack **st_a, t_stack **st_b);
+void	checker(t_stack **st_a, t_stack **st_b);
+void	exit_and_print(int result, t_stack **st_a, t_stack **st_b);
+
+#endif
